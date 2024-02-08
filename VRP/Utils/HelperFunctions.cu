9c4bd292ad0428b26957ce17ab29af1b2ad5ec4c
AStruthers2000 <struthers919@gmail.com>
2024-02-08 08:05:12 -0800

    Deleted old commented code and updated comment for Host_PrintArray

--- VRP/Utils/HelperFunctions.cu
+++ VRP/Utils/HelperFunctions.cu
@@ -8,24 +8,12 @@
 
 using namespace std;
 
-
-/*
-template <typename T> [[maybe_unused]] __host__ void CustomUtils_Host_PrintArray(T* arr, size_t n, int precision = 4)
-{
-    cout << "[";
-    for(size_t i = 0; i < n - 1; i++)
-    {
-        cout << std::setprecision(precision) << arr[i] << ", ";
-    }
-    cout << arr[n - 1] << "]" << endl;
-}
- */
 /**
  * \brief Helper function to print a host array easily.
  *
  * This function prints a host array to the screen using std::cout.
  * This will only work when running code on the host, and will only work
- * with a host array.
+ * with a host array.\n\n
  *
  * NOTE: If calling this function gives an LNK1120 error, you must call it
  * with HelperFunctions::Host_PrintArray<[YOUR DESIRED TYPE]>(array, n). This
